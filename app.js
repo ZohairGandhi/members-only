3d13083ee98d6747f984e1da7774f99dad518108
Zohair Gandhi <87702549+ZohairGandhi@users.noreply.github.com>
2025-10-12 20:55:40 +1100

    chore: init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Test"));
+
+const PORT = 3000;
+
+app.listen(3000, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Listening on port ${PORT}`);
+});
